cd7abd49cb1da922d73d32b2dc6a849aa7a57f59
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2024-12-27 20:57:41 +0500

    added services and trusted comp part

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
 import HeroSection from "../components/UI/HeroSection";
+import Services from "../components/UI/Services";
+import Trusted from "../components/UI/Trusted";
 
 export const Home = () => {
   const data = {
     name: "imran store",
   };
-  return <HeroSection myData={data} />;
+  return (
+    <>
+      <HeroSection myData={data} />
+      <Services />
+      <Trusted />
+    </>
+  );
 };
