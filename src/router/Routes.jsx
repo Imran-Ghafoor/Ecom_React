8d70473a6b3431407b15134446630ccc8e2f14cc
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2024-12-27 17:12:50 +0500

    setup routes/pages

---
+++ src/router/Routes.jsx
@@ -0,0 +1,46 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "../components/Layout/Layout";
+import { Home } from "../pages/Home";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Cart from "../pages/Cart";
+import Products from "../pages/Products";
+import SingleProduct from "../pages/SingleProduct";
+
+const Routes = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+        },
+        {
+          path: "/cart",
+          element: <Cart />,
+        },
+        {
+          path: "/products",
+          element: <Products />,
+        },
+        {
+          path: "/singleproduct/:id",
+          element: <SingleProduct />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
+};
+
+export default Routes;
